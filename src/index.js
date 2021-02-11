58a9bd1ccc0920611f84deda2ffadb04101a4253
Harish Boke <harish.boke@gmail.com>
2021-02-11 17:12:28 +0530

    [CHANGE] SALES-29 #time 0h 30m Enable playground

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ connectDb();
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers,
-  context: {models}
+  context: { models },
+  introspection: true,
+  playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
